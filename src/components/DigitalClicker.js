7cc5d82ea793cf280ebdfbf9143ff240175356b9
github username <github email address>
2019-09-18 10:43:30 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+export default class DigitalClicker extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = (event) => {
+        const increasedThing = this.state.timesClicked += 1
+        this.setState({
+            timesClicked: increasedThing
+        })
+    }
+    
+    render() {
+        console.log(this.state.timesClicked)
+        return <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,52 @@
 // Code YouTubeDebugger Component Here
+// Code DigitalClicker Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+    }
+
+    handleYouTubeClick = (event) => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+  
+    handleResolutionClick = (event) => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                ...this.state.settings.video,
+                resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render() {
+        console.log(this.state.settings.video)
+        return (
+        <div>
+        <button onClick={this.handleYouTubeClick} className="bitrate">YouTube Button</button>
+        <button onClick={this.handleResolutionClick} className="resolution">Resolution Button</button>
+        </div>
+        )
+    }
+}
